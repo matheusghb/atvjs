2d65563e26796e7f35a9ccc2bb2f1df36a19def2
matheusghb <mistermquack124@gmail.com>
2024-09-25 17:46:20 -0300

    1

---
+++ codigo.js
@@ -0,0 +1,15 @@
+function abriradicionar() {
+    Element.createElement ('input type="text" name="fname" placeholder="Título:" id="ti"');
+}
+
+'<div class="grid">'
+    '<div class="g">Homestuck</div>'
+    '<div class="g">Pitoca Grossa</div>'
+    '<div class="g">2009</div>'
+    '<div class="g">Sci-fi</div>'
+
+'<input type="text" name="fname" placeholder="Título:" id="ti">'
+'<input type="text" name="fname" placeholder="Autor:" id="au">'
+'<input type="text" name="fname" placeholder="Ano de Publicação:" id="an">'
+'<input type="text" name="fname" placeholder="Gênero" id="ge">'
+'<button type="button" id="co">Confirmar</button>'
