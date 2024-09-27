8d850afdfe6ac1e4a96e67ca515214b7332dce0f
KirbyBucciarati <figor3335@gmail.com>
2024-09-27 00:43:41 -0300

    pedro igor

--- codigo.js
+++ codigo.js
@@ -1,6 +1,9 @@
 let lista = []
 
 document.getElementById('ad').addEventListener('click', function() {
+
+    document.getElementById('fu').innerHTML = ''
+
     const ti = document.createElement('input');
     ti.type = 'text';
     ti.name = 'fname';
@@ -39,7 +42,6 @@ document.getElementById('ad').addEventListener('click', function() {
         document.getElementById('au').value = '';
         document.getElementById('an').value = '';
         document.getElementById('ge').value = '';
-        console.log(lista)
     }
 
     document.getElementById('fu').appendChild(ti);
@@ -49,14 +51,55 @@ document.getElementById('ad').addEventListener('click', function() {
     document.getElementById('fu').appendChild(co);
 });
 
-'<div class="grid">'
-    '<div class="g">Homestuck</div>'
-    '<div class="g">Pitoca Grossa</div>'
-    '<div class="g">2009</div>'
-    '<div class="g">Sci-fi</div>'
+document.getElementById('ex').addEventListener('click', function(){
 
-'<input type="text" name="fname" placeholder="Título:" id="ti">'
-'<input type="text" name="fname" placeholder="Autor:" id="au">'
-'<input type="text" name="fname" placeholder="Ano de Publicação:" id="an">'
-'<input type="text" name="fname" placeholder="Gênero" id="ge">'
-'<button type="button" id="co">Confirmar</button>'
+    document.getElementById('fu').innerHTML = ''
+
+    const ser = document.createElement('input');
+    ser.type = 'text';
+    ser.name = 'fname';
+    ser.placeholder = 'Nome de livro:';
+    ser.id = 'ser'
+
+    const edi = document.createElement('h1');
+    edi.id = 'edi'
+    edi.innerText = 'Digite o livro que deseja excluir.'
+
+    const con = document.createElement('button');
+    con.type = 'button';
+    con.id = 'con'
+    con.textContent = 'Confirmar'
+    con.onclick = function() {
+        const inf = document.getElementById('ser').value
+        if (lista.includes(inf)) {
+            num = lista.indexOf(inf)
+            lista.splice(num, 4)
+            console.log(num,num+4)
+            document.getElementById('edi').innerText = 'Livro foi excluído da tabela.'
+        }
+        else {
+            document.getElementById('edi').innerText = 'Livro não foi encontrado.'
+        }
+    }
+
+    document.getElementById('fu').appendChild(ser)
+    document.getElementById('fu').appendChild(edi)
+    document.getElementById('fu').appendChild(con)
+});
+
+document.getElementById('li').addEventListener('click', function(){
+
+    document.getElementById('fu').innerHTML = ''
+
+    const grid = document.createElement('div');
+    grid.className='grid';
+    grid.id = 'grid'
+    document.getElementById('fu').appendChild(grid); 
+    let i, len, text;
+    for (let i = 0, len = lista.length, text = ''; i < len; i++) {
+        let g = document.createElement('div');
+        g.className = 'g'+lista[i];
+        g.textContent = lista[i];
+        document.getElementById('grid').appendChild(g);
+    }
+})
